86247cad0b54a2e0862b1ff161ff97bce6ffc3cf
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-01-14 22:06:03 +0530

    Update request headers in getStream and getInfo functions

--- controllers/getStream.ts
+++ controllers/getStream.ts
@@ -14,6 +14,24 @@ export default async function getStream(req: Request, res: Response) {
   try {
     const link = await axios.get(`${process.env.BASE_URL}/playlist/${path}`, {
       headers: {
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "no-cache",
+        "Content-Length": "0",
+        "Content-Type": "application/x-www-form-urlencoded",
+        Dnt: "1",
+        Origin: "https://friness-cherlormur-i-275.site",
+        Pragma: "no-cache",
+        "Sec-Ch-Ua":
+          '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
         "X-Csrf-Token": key,
         Referer: `https://google.com/`,
       },
--- lib/getInfo.ts
+++ lib/getInfo.ts
@@ -4,15 +4,24 @@ export default async function getInfo(id: string) {
   try {
     const response = await axios.get(`${process.env.BASE_URL}/play/${id}`, {
       headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-        Accept: "*/*",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Encoding": "gzip, deflate, br",
-        Connection: "keep-alive",
+        "Accept-Language": "en-US,en;q=0.9",
+        DNT: "1",
+        "sec-ch-ua":
+          '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
         Origin: "https://allmovieland.fun/",
         Referer: "https://google.com",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
       },
     });
     const $ = cheerio.load(response.data);
